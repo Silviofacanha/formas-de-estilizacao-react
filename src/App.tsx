a4042ccfa72bf7f4bfbd2ddd0329f27e471c8b3f
SilvioGFacanha <silvio.facanha@soitic.com>
2023-02-20 11:47:34 -0300

    aula 01

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+export const App = () => {
+    const [padding, setPadding] = useState(0);
+
+    useEffect(() => {
+        /*setInterval(() => {
+            setPadding((oldPadding) => {
+                return oldPadding + 1;
+            });
+        }, 2000);*/
+    });
+
+    return (
+        <div
+            style={{
+                backgroundColor: 'black',
+                color: 'white',
+                padding: padding,
+            }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+            Teste
+        </div>
+    );
+};
